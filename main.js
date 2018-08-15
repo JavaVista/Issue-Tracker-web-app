4e7100f269390d76a16d39c4d13471d497b2d319
Javi Carrion <suavejavi@google.com>
2018-08-15 18:51:20 -0400

    Add issue data code with variable to fetchIssues() function

--- main.js
+++ main.js
@@ -2,5 +2,25 @@ function fetchIssue() {
     const issues = JSON.parse(localStorage.getItem('issues'));
     const issuesList = document.getElementById('issuesList');
 
-    
+    issuesList.innerHTML = '';
+
+    for (let index = 0; index < issues.length; index++) {
+        const id = issues[index].id;
+        const description = issues[index].text;
+        const priority = issues[index].priority;
+        const assigment = issues[index].assignedTo;
+        const status = issues[index].status;
+
+        issuesList.innerHTML += `<div class="well">
+        <h6>Issue ID: ${id}</h6>
+        <p><span class="label label-info">${status}</span></p>
+        <h3>${description}</h3>
+        <p><span class="glyphicon glyphicon-time"></span> ${priority} <span class="glyphicon glyphicon-user"></span> ${assigment}</p>
+        <a href="#" class="btn btn-warning" onclick="setStatusClosed( ${id} )">Close</a>
+        <a href="#" class="btn btn-danger" onclick+"deleteIssue( ${id} )">Delete</a>
+        </div>`;
+
+
+    }
+
 }
